6f9d700b7da2d0a44d6177b424991690710b4458
johann-miller <johann.miller@protonmail.com>
2020-01-23 15:39:21 -0500

    Added small feature to select gear from list

--- src/components/Loadout.svelte
+++ src/components/Loadout.svelte
@@ -1,19 +1,19 @@
 <script>
-    let slots = [
-        'helm',
-    ]
     let options = [
-        'option',
-        'opti',
-        'op',
-        'o'
+        'void melee helm',
+        'serpentine visage',
+        'archer\'s helm',
+        'helm of neitiznot'
     ]
     let filteredOptions = options
     let equipment = ""
+    let equipmentInput
+    let showOptions = false
+    let helm = ""
 
     function filterOptions() {
         let array = []
-        let input = equipment
+        let input = equipmentInput
         input = input.toUpperCase()
 
         function filter(item, index) {
@@ -26,6 +26,14 @@
         options.forEach(filter)
         filteredOptions = array
     }
+
+    function openOptions() {
+        showOptions = true
+    }
+
+    function setEquipment(value) {
+        equipment = value
+    }
 </script>
 
 <style>
@@ -34,16 +42,38 @@
         flex-flow: column;
         align-items: flex-start;
     }
+
+    ul {
+        list-style-type: none;
+    }
+
+    button {
+        border: 1px #3f3e3c solid;
+        background: #fcfcfc;
+        color: #3f3e3c;
+    }
 </style>
 
 <h1>Loadout</h1>
 
+<ul>
+    <li>
+        <button on:click="{openOptions}">
+            <span>Helm</span>
+            <span>{equipment}</span>
+        </button>
+    </li>
+</ul>
+{#if showOptions}
 <form action="" onsubmit="event.preventDefault()" class="loadout">
     <label for="equipment">Equipment</label>
-    <input type="text" id="equipment" on:keyup="{filterOptions}" bind:value="{equipment}">
+    <input type="text" id="equipment" on:keyup="{filterOptions}" bind:value="{equipmentInput}" autocomplete="off">
     <ul>
         {#each filteredOptions as option}
-            <li>{option}</li>
+            <li>
+                <button on:click="{setEquipment(option)}">{option}</button>
+            </li>
         {/each}
     </ul>
 </form>
+{/if}
